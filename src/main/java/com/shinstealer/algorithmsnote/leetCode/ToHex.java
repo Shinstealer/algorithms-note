94a1af4a9363f781ee9d89bff5941d98e96c73c2
shinstealer <coolldd890@gmail.com>
2020-07-09 17:36:13 +0900

    fix

--- src/main/java/com/shinstealer/algorithmsnote/leetCode/ToHex.java
+++ src/main/java/com/shinstealer/algorithmsnote/leetCode/ToHex.java
@@ -7,7 +7,7 @@ public class ToHex {
     public static void main(String[] args) {
         String hex = String.format("%04x", new BigInteger(1,"text".getBytes()));
 
-        String shardId = "shardId-" + String.format("%010x", 2);
+        String shardId = "shardId-" + String.format("%010d", 2);
 
         System.out.println(hex);
 
